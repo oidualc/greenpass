c41e5cfacab7e0dc8db331fe56b07d74a344342b
Claudio Cocciarelli <claudiococciarelli@gmail.com>
2022-01-08 21:34:44 +0100

    fix: image height

--- src/screens/GreenPass.tsx
+++ src/screens/GreenPass.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Dimensions, Image, ScrollView } from "react-native";
 
+const image = require("../assets/greenpass.jpeg");
+const source = Image.resolveAssetSource(image);
+const windowWidth = Dimensions.get("window").width;
+const heightToWidthRatio = source.height / source.width;
+const componentHeight = windowWidth * heightToWidthRatio;
+
 export default function GreenPass() {
   return (
     <ScrollView>
       <Image
-        source={require("../assets/greenpass.jpeg")}
+        source={image}
         style={{
           alignSelf: "center",
           maxWidth: Dimensions.get("window").width,
+          height: componentHeight,
         }}
         resizeMode="contain"
       />
